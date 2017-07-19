6922665d9e8342ce5cbf02edaf15518448665da9
ChoiJongMin <choijongmin9@gmail.com>
2017-07-19 17:36:09 +0900

    save data into dbms

--- project.py
+++ project.py
@@ -3,6 +3,11 @@ import urllib2
 import urllib
 import time
 import datetime
+import pymysql as sql
+
+conn = sql.connect(host = '192.168.0.14', user = 'pi', password = 'ziumks00', db = 'ZiumIOT' , charset = 'utf8')
+curs = conn.cursor()
+
 
 ser = serial.Serial('/dev/ttyUSB0', 9600) # connection with arduino using 485
 url = 'http://192.168.0.14:9900/data' # address of server
@@ -12,13 +17,20 @@ def fileSave(data) :
 	with open("../data/data.txt",'a') as f : # save data in a folder named 'data'
 		f.write(str(now) + str(data) + '\n')
 
+def dbSave(data) :
+	query = "Insert into data(temperature, humidity) values (%s,%s);"
+	curs.execute(query, (data['temperature'],data['humidity']))
+	conn.commit()	
 
 
 def readData() :
 	data = (ser.readline()) #read data from arduino
-	data = data.split(',')
-	data = {'temperature':data[0],'humidity':data[1]}
+	data = data.replace('\n','').replace('\r','')
+	data = data.split(' ')
+	data = {'temperature': float(data[0]), 'humidity': float(data[1])}
+	dbSave(data)
 	fileSave(data) #save data immediately
+
 	return (data)
 
 
@@ -33,3 +45,4 @@ def trsData() :
 
 trsData()
 
+conn.close()
