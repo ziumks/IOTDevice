cd9ad59fd9ee837a35a1da961d109e9027ebb288
cjm0904 <hfo@nate.com>
2017-07-19 10:41:58 +0900

    Add files via upload

---
+++ sketch_jul18a.ino
@@ -0,0 +1,26 @@
+#include <DHT11.h>
+#define pin 2
+DHT11 dht11(pin);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while(!Serial){
+    
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int err;
+  float temperature, humidity;
+  String msg;
+  if(!(err=dht11.read(humidity,temperature))){
+    msg = String(temperature) + "," + String(humidity);
+    Serial.print(msg);
+    delay(5000);
+  }else{
+    Serial.println("Error : " + err);
+    delay(500);
+  }
+}
